7dd8a177e1e08e070278c9bee90e6dcf64d64db5
LeoD3v <leoargusa1991@gmail.com>
2023-09-15 20:07:48 +0200

    admin db page

--- public/js/script.js
+++ public/js/script.js
@@ -2,3 +2,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("DataDine JS imported successfully!");
 });
+window.onscroll = function () {
+  const formFields = document.querySelector(".form-style");
+  const scrollPosition = window.scrollY || window.pageYOffset;
+
+  if (scrollPosition > 208) {
+    formFields.classList.add("sticky");
+  } else {
+    formFields.classList.remove("sticky");
+  }
+};
