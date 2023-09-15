640e49347eb5d7ffd11a63cdb8d9035325e653a1
LeoD3v <leoargusa1991@gmail.com>
2023-09-15 21:36:13 +0200

    order page

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,14 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
 });
-
-module.exports = router;
-
-
-
-
+router.get("/order", (req, res) => {
+  res.render("track-order");
+});
 // this is a test comment to see if I can push to github
+module.exports = router;
--- routes/orders.routes.js
+++ routes/orders.routes.js
@@ -1,5 +1,23 @@
 const express = require("express");
 const router = express.Router();
 module.exports = router;
-
-
+//const progressRound = window.querySelector(‘.progress-round’);
+// Function to update the progress indicator position
+// function updateProgressIndicator(status) {
+//   switch (status) {
+//     case 'Confirmed':
+//       progressRound.style.left = '0%';
+//       break;
+//     case 'Preparation':
+//       progressRound.style.left = '25%';
+//       break;
+//     case 'On the way':
+//       progressRound.style.left = '50%';
+//       break;
+//     case 'Delivered':
+//       progressRound.style.left = '75%';
+//       break;
+//     default:
+//       break;
+//   }
+// }
