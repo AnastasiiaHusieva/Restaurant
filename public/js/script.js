0cee45c2e068be3c2b4afc18e7c5a17e5f3888f4
John Doe <johndoe@example.com>
2023-09-17 01:31:47 +0200

    commented the scroll function

--- public/js/script.js
+++ public/js/script.js
@@ -2,18 +2,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("DataDine JS imported successfully!");
 });
-document.addEventListener("DOMContentLoaded", function () {
-  window.onscroll = function () {
-    const formFields = document.querySelector(".form-style");
-    const scrollPosition = window.scrollY || window.pageYOffset;
-
-  if (scrollPosition > 208) {
-    formFields.classList.add("sticky");
-  } else {
-    formFields.classList.remove("sticky");
-  }
-};
-})
+//document.addEventListener("DOMContentLoaded", function () {
+//  window.onscroll = function () {
+//    const formFields = document.querySelector(".form-style");
+//    const scrollPosition = window.scrollY || window.pageYOffset;
+//
+//  if (scrollPosition > 208) {
+//    formFields.classList.add("sticky");
+//  } else {
+//    formFields.classList.remove("sticky");
+//  }
+//};
+//})
 
 
 
