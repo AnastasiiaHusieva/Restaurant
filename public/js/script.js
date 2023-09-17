c932ad02a9f56a472811f05026c8c5a5e43a2cbb
LeoD3v <leoargusa1991@gmail.com>
2023-09-17 22:19:30 +0200

    menu database for admin profiles

--- public/js/script.js
+++ public/js/script.js
@@ -10,18 +10,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
     if (scrollPosition > 220) {
       formFields.classList.add("sticky");
-      tableheader.classList.add("sticky-header");
-      description.classList.add("spacebetween");
-      name.classList.add("nameSpacebetween");
-      price.classList.add("betweenPrice");
-      title.classList.add("titlestick");
     } else {
       formFields.classList.remove("sticky");
-      tableheader.classList.remove("sticky-header");
-      description.classList.remove("spacebetween");
-      name.classList.remove("nameSpacebetween");
-      price.classList.remove("betweenPrice");
-      title.classList.remove("titlestick");
     }
   };
 });
