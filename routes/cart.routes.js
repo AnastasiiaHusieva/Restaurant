990c3e8757dadce212e6ec905c6e4ae1a52dfc8b
John Doe <johndoe@example.com>
2023-09-22 03:19:03 +0200

    cart is working

--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -1,8 +1,33 @@
 const express = require("express");
 const router = express.Router();
 const Item = require("../models/ItemsAdmin.model");
+const User = require("../models/User.model");
 router.get("/", (req, res) => {
-  res.render("user/cart");
+  console.log("hola!!");
+  res.render("cart").then((data) => {
+    console.log("ahahahah", data);
+  });
 });
-
+router.post("/", async (req, res) => {
+  const userId = req.session.currentUser._id;
+  const { itemId } = req.body; // Assuming you have an itemId from the form
+  try {
+    // Update the user's cart by pushing the itemId (ObjectId) into the cart array
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { $push: { cart: itemId } }, // Use $push to add the itemId to the cart array
+      { new: true }
+    );
+    if (!updatedUser) {
+      console.error("User not found");
+      return res.status(404).json({ message: "User not found" });
+    }
+    // Optionally, you can send a response back to the client to indicate success
+    console.log("Updated user:", updatedUser);
+    res.render("cart", { cartItem: itemId });
+  } catch (error) {
+    console.error("Error updating user's cart:", error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+});
 module.exports = router;
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -34,33 +34,33 @@ router.get("/", (req, res, next) => {
 // this is a test comment to see if I can push to github
 
 // cart routes
-router.get("/cart", (req, res) => {
-  res.render("cart");
-});
+// router.get("/cart", (req, res) => {
+//   res.render("cart");
+// });
+// 
+// router.post("/cart", (req, res) => {
+//   const { itemName, itemPrice, itemImageURL, itemQuantity } = req.body;
+//   const item = { itemName, itemPrice, itemImageURL, itemQuantity };
+//   req.session.cart.push(item);
+//   res.redirect("/cart");
+// });
 
-router.post("/cart", (req, res) => {
-  const { itemName, itemPrice, itemImageURL, itemQuantity } = req.body;
-  const item = { itemName, itemPrice, itemImageURL, itemQuantity };
-  req.session.cart.push(item);
-  res.redirect("/cart");
-});
-
-router.get("/items/:itemCategory", (req, res, next) => {
-  Item.find({ itemCategory: req.params.itemCategory })
-    .then((items) => {
-      if (!items) {
-        return res
-          .status(404)
-          .json({ message: "No items found for this category." });
-      }
-      res.json(items);
-      console.log("hello! items");
-    })
-    .catch((error) => {
-      console.error("Error fetching items:", error);
-      res.status(500).json({ message: "Internal server error." });
-    });
-});
+// router.get("/items/:itemCategory", (req, res, next) => {
+//   Item.find({ itemCategory: req.params.itemCategory })
+//     .then((items) => {
+//       if (!items) {
+//         return res
+//           .status(404)
+//           .json({ message: "No items found for this category." });
+//       }
+//       res.json(items);
+//       console.log("hello! items");
+//     })
+//     .catch((error) => {
+//       console.error("Error fetching items:", error);
+//       res.status(500).json({ message: "Internal server error." });
+//     });
+// });
 
 router.get("/checkout", (req, res) => {
   let isLoggedIn = req.session.currentUser;
