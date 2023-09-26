fc3d9a8cfa9d3a039957635bcfa078ea8835067c
John Doe <johndoe@example.com>
2023-09-27 00:48:06 +0200

    checkout js and css for payment methods+breakpoint

--- public/js/checkout.js
+++ public/js/checkout.js
@@ -1,28 +1,30 @@
-    const paymentCard = document.querySelector(".payment-card");
-    const card = document.querySelector(".card");
-    const paymentPaypal = document.querySelector(".payment-paypal");
-    const paypal = document.querySelector(".paypal");
+const paymentCard = document.querySelector(".payment-card");
+const card = document.querySelector(".card");
+const paymentPaypal = document.querySelector(".payment-paypal");
+const paypal = document.querySelector(".paypal");
+const input = document.querySelectorAll("input");
 document.addEventListener("DOMContentLoaded", () => {
 
-  
-   paymentCard.addEventListener("click", () => {
-       if (card.style.display === "block") {
-         card.style.display = "none";
-         console.log(" card none ")
-       } else {
-         card.style.display = "block";
-         console.log(" card block ")
-       }
-     });
-       
-    paymentPaypal.addEventListener("click", () => {
-      if (paypal.style.display === "block") {
-        paypal.style.display = "none";
-        console.log(" card none ")
-      } else {
-        paypal.style.display = "block";
-        console.log(" card block ")
-      }
-     });
-   });
+
+paymentCard.addEventListener("click", () => {
+  paypal.style.display = "none";
+   if (card.style.display === "block") {
+     card.style.display = "none";
+     console.log(" card none ")
+   } else {
+     card.style.display = "block";
+     console.log(" card block ")
+   }
+ });
    
+paymentPaypal.addEventListener("click", (event) => {
+   card.style.display = "none";
+  if (paypal.style.display === "block") {
+    paypal.style.display = "none";
+    console.log(" card none ")
+  } else {
+    paypal.style.display = "block";
+    console.log(" card block ")
+  }
+ });
+});
--- routes/orders.routes.js
+++ routes/orders.routes.js
@@ -3,99 +3,43 @@ const router = express.Router();
 const Order = require("../models/Order.model");
 const User = require("../models/User.model");
 
-const isLoggedIn = require("../middleware/isLoggedIn");
-
 router.get("/", (req, res) => {
   const userId = req.session.currentUser._id;
 
-  Order.findOne({ contact: userId }).then((orderItems) => {
-    console.log("AAAAAHAHAHAHAHAHAHAHAHAHAH", orderItems);
-    if (!orderItems) {
-      User.findOneById(userId);
-      console
-        .log("UUUUUUSSSSSSEEEEEEERRRRRRRRRIIIIIDDDDD", userId)
-        .populate("cart")
-        .then((userObject) => {
-          if (!userObject) {
-            return res.status(404).json({ message: "User not found" });
-          }
-          const orderData = {
-            contact: userObject._id,
-            items: userObject.cart,
-          };
-          Order.create(orderData)
-            .then((order) => {
-              if (order) {
-                console.log("!!!!!!!!!", order);
-                User.findOneAndUpdate(
-                  { _id: userId },
-                  { $set: { cart: [] } },
-                  { new: true }
-                ).then((updatedCart) => {
-                  if (updatedCart) {
-                    res.render("track-order", { order });
-                  }
-                });
-              } else console.log("the order was not created");
-            })
-            .catch((error) => {
-              console.error("Error creating order:", error);
-              res.status(500).json({ message: "Internal Server Error" });
+  User.findById(userId)
+    .populate("cart")
+    .then((userObject) => {
+      const orderData = {
+        contact: userObject._id,
+
+        items: userObject.cart,
+      };
+
+      Order.create(orderData)
+        .then((order) => {
+          if (order) {
+            console.log("!!!!!!!!!", order);
+            User.findOneAndUpdate(
+              { _id: userId },
+              { $set: { cart: [] } },
+              { new: true }
+            ).then((updatedCart) => {
+              if (updatedCart) {
+                res.render("track-order", { order });
+              }
             });
+          } else console.log("the order was not created");
         })
-        .catch((err) => {
-          console.error("Error finding user:", err);
+        .catch((error) => {
+          console.error("Error creating order:", error);
           res.status(500).json({ message: "Internal Server Error" });
         });
-    } else {
-      res.render("track-order", { orderItems });
-    }
-  });
+    })
+    .catch((err) => {
+      console.error("Error finding user:", err);
+      res.status(500).json({ message: "Internal Server Error" });
+    });
 });
-
-// //////////////////////////////////////////////////
-
-// router.get("/", isLoggedIn, (req, res) => {
-//   const userId = req.session.currentUser._id;
-
-//   User.findById(userId)
-//     .populate("cart")
-//     .then((userObject) => {
-//       const orderData = {
-//         contact: userObject._id,
-
-//         items: userObject.cart,
-//       };
-
-//       Order.create(orderData)
-
-//         .then((order) => {
-//           if (order) {
-//             console.log("!!!!!!!!!", order);
-//             User.findOneAndUpdate(
-//               { _id: userId },
-//               { $set: { cart: [] } },
-//               { new: true }
-//             ).then((updatedCart) => {
-//               if (updatedCart) {
-//                 Order.find({ contact: userId }).then((orderItems) => {
-//                   console.log("RRRRRRRRRRRRRR", orderItems.items);
-//                   res.render("track-order", { orderItems });
-//                 });
-//               }
-//             });
-//           } else console.log("the order was not created");
-//         })
-//         .catch((error) => {
-//           console.error("Error creating order:", error);
-//           res.status(500).json({ message: "Internal Server Error" });
-//         });
-//     })
-//     .catch((err) => {
-//       console.error("Error finding user:", err);
-//       res.status(500).json({ message: "Internal Server Error" });
-//     });
-// });
 
 module.exports = router;
 //const progressRound = window.querySelector(‘.progress-round’);
