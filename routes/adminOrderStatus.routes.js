90d6822159f73279ff43637aa76b7dce3c803634
LeoD3v <leoargusa1991@gmail.com>
2023-10-06 00:21:37 +0200

    order admin

--- routes/adminOrderStatus.routes.js
+++ routes/adminOrderStatus.routes.js
@@ -4,10 +4,12 @@ const Order = require("../models/Order.model");
 const User = require("../models/User.model");
 const isAdmin = require("../middleware/isAdmin");
 
-router.get("/",isAdmin, (req, res, next) => {
+router.get("/", isAdmin, (req, res) => {
   Order.find().then((userOrders) => {
     console.log("ACA ACA ACA !!!!!!!", userOrders);
-    res.render("admin/adminOrderStatus", { userOrders });
+    const cart = userOrders.map((order) => order.items);
+    console.log(cart);
+    res.render("admin/adminOrderStatus", { userOrders, cart });
   });
 });
 module.exports = router;
